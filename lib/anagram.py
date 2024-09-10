272df196fc1beee2e11952587bfdcf15ccbb371a
gideonkkk <gideon.kariuki@student.moringaschool.com>
2024-09-10 08:33:34 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram :
+    def __init__(self, word):
+        self.word = word.lower()
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+        for word in word_list:
+            if sorted(word.lower()) == sorted_word:
+                matches.append(word)
+        return matches
+finder = Anagram("finder")
+print(finder.match(['friend', 'refind', 'rindef', 'fired']))
+
